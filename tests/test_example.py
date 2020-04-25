f1894829156dacc5181190a0556fdfaf6bf8a04a
brodde <benoitrodde33@gmail.com>
2020-04-25 08:18:10 +0200

    pytest

--- tests/test_example.py
+++ tests/test_example.py
@@ -11,20 +11,20 @@ def test_ok():
     print("ok")
 
 
-def test_fail():
-    assert 0
+# def test_fail():
+#     assert 0
 
 
-def test_error(error_fixture):
-    pass
+# def test_error(error_fixture):
+#     pass
 
 
-def test_skip():
-    pytest.skip("skipping this test")
+# def test_skip():
+#     pytest.skip("skipping this test")
 
 
-def test_xfail():
-    pytest.xfail("xfailing this test")
+# def test_xfail():
+#     pytest.xfail("xfailing this test")
 
 
 @pytest.mark.xfail(reason="always xfail")
